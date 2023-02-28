bb18c82ff4b7be38274b8464fa4a8d228b878c01
timfuhrmann <tf@becklyn.com>
2023-02-28 12:49:11 +0100

    Add lint to CI

--- src/css/helper/hierarchy.ts
+++ src/css/helper/hierarchy.ts
@@ -1,6 +1,6 @@
 const hierarchy = ["body"] as const;
 
-type Hierarchy = typeof hierarchy[number];
+type Hierarchy = (typeof hierarchy)[number];
 
 export const zIndex = hierarchy.reduce((acc, current, index) => {
     acc[current] = index;
